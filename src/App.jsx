33345a896c0e9e807a271ae47ecc0f3b7e5ef15d
Rohankoley <142784160+Rohankoley@users.noreply.github.com>
2024-05-16 14:03:59 +0530

    push app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,66 @@
-
+import {useState,useEffect } from 'react'
+import fetchApi from './Components/FetchData'
 import './App.css'
 
 function App() {
+  const [text, setText] = useState('')
+  const [data,setData] = useState('')
+  const [currentUrl, setCurrentUrl] = useState('');
+
+
+  
+  useEffect(() => {
+    // Function to retrieve URL when component mounts or updates
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      const url = activeTab.url;
+      const problemName = url.substring(url.indexOf("problems/") + 9, url.indexOf("/", url.indexOf("problems/") + 9));
+      // Complete code
+   
+      setCurrentUrl(problemName);
+    });
+   
+
+  }, [currentUrl, setCurrentUrl]);
+
+  const getCompletion = async ()=>{
+    try {
+      if(text===""){
+        const fetchData = `Give me an approach code to ${currentUrl} solve type of problem `
+        const result = await fetchApi(fetchData);
+        setData(result)
+      }else{
+        const result = await fetchApi(text);
+        setData(result)
+        
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+  
+  }
+useEffect(()=>{
+  getCompletion()
+},[text, setText])
+
+
 
   return (
     <div>
-      <h1 className='text-3xl'>Hello World</h1>
-      <h1></h1>
-      <textarea className='input_field' name="" id="">
-        
-      </textarea>
+      <p >Leetcode Gpt</p>
+      <textarea  value={text} onChange={(e)=>setText(e.target.value)}/>
+    <button  onClick={getCompletion}>Submit</button>
+    {data?<div className='answer-box' >
+      {data}
+    </div>:null}
+
+
     </div>
   )
 }
 
 export default App
+
+
+
--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,19 @@
-import{GooglenerativeAI} from "@google/generative-ai";
-const  genAI = new GooglenerativeAI("AIzaSyDOA85O_3T3bHf7UfZJdK9PH9dCYKYdb_k");
-const fetchApi = async(prompt)=>{
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+import { GoogleGenerativeAI } from "@google/generative-ai";
+// import { VertexAI } from "@google-cloud/vertexai";
+const genAI = new GoogleGenerativeAI("AIzaSyDOA85O_3T3bHf7UfZJdK9PH9dCYKYdb_k");
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text)
-    return text;
-    
+const fetchApi = async (prompt)=>{
+// Access your API key as an environment variable (see "Set up your API key" above)
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+const result = await model.generateContent(prompt);
+const response = await result.response;
+const text = response.text();
+console.log(text)
+return text;
+
 }
+
+
 export default fetchApi;
